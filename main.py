1e2b136f2fefac4aceb31833cff19a73572b879d
Beatriz Lisboa <btrzlisboa037@gmail.com>
2023-01-07 18:04:58 +0000

    mudanca ordem defs

--- main.py
+++ main.py
@@ -167,6 +167,49 @@ def panel_login():
 
     window_login.place(x=0, y=0)
 
+## - - - - - - - - - - CONTAINER ADMIN PANEL - - - - - - - - - - ##
+
+def panel_admin():
+    global currentpanel
+    currentpanel.pack_forget()
+
+    painel_adm = PanedWindow(window, width=1080, height=720)
+    painel_adm.place(x=0,y=0)
+    currentpanel = painel_adm
+    painel_adm.configure(bg = "#d3d3d3")
+    
+    # imagem e infos do user
+    userImg = PhotoImage(file= "./imgs/painel_adm/user.png")
+    labelImg = Label(painel_adm, image=userImg, width=180, height=180, bd=0, bg="#d3d3d3")
+    labelImg.place(x= 420, y=100)
+
+    admTxt = Label(painel_adm, text="Função: administrador", width=24, height=3, bd=0, bg="#d3d3d3", fg="black")
+    admTxt.place(x=420, y=345)
+
+    # botão voltar
+    btnVoltar = Button(painel_adm, text="Voltar", width=10, command=panel_account)
+    btnVoltar.place(x=30, y=30)
+
+    # botão add album
+    btnAddAlbum = Button(painel_adm, text="Adicione um álbum", width=25)
+    btnAddAlbum.place(x=300, y=480)
+
+    # botão remover album
+    btnRemovAlbum = Button(painel_adm, text="Remova um álbum", width=25)
+    btnRemovAlbum.place(x=520, y=480)
+
+    # botão usuarios
+    btnUsers = Button(painel_adm, text="Veja usuários", width=25)
+    btnUsers.place(x=300, y=525)
+
+    # botão apagar reviews
+    btnApagarReviews = Button(painel_adm, text="Apague um review", width=25)
+    btnApagarReviews.place(x=520, y=525)
+
+    # botão log out
+    btnLogOut = Button(painel_adm, text="Log out", width=10, command=logout_and_change_page(panel_homepage))
+    btnLogOut.place(x=960, y=670)
+
 ## - - - - - - - - - - CONTAINER ACCOUNT - - - - - - - - - - ##
 
 def panel_account():
@@ -211,6 +254,35 @@ def panel_account():
         btn_logout.place(x=800, y=90)
 
     window_account.place(x=0, y=0)
+
+
+## - - - - - - - - - - CONTAINER NOTIFICACOES - - - - - - - - - - ##
+
+def panel_notific():
+    global currentpanel
+    currentpanel.pack_forget()
+
+    panel_notific = PanedWindow(window, width=1080, height=720)
+    currentpanel = panel_notific
+    panel_notific.configure(bg = "#d3d3d3")
+
+    txt = Label(panel_notific, text="Isto é uma notificacao", width=24, height=3, bd=0, bg="#d3d3d3", fg="black")
+    txt.place(x=420, y=345) 
+
+## - - - - - - - - - - CONTAINER SEARCH - - - - - - - - - - ##
+
+def panel_search():
+    global currentpanel
+    currentpanel.pack_forget()
+
+    panel_search = PanedWindow(window, width=1080, height=720)
+    currentpanel = panel_search
+    panel_search.configure(bg = "#d3d3d3")
+
+
+    txt = Label(panel_notific, text="Pesquise aqui", width=24, height=3, bd=0, bg="#d3d3d3", fg="black")
+    txt.place(x=420, y=345)
+
 
 ## - - - - - - - - - - CONTAINER HOMEPAGE - - - - - - - - - - ##
 
@@ -279,76 +351,6 @@ def panel_homepage():
 
 
     home_page.mainloop()
-
-## - - - - - - - - - - CONTAINER ADMIN PANEL - - - - - - - - - - ##
-
-def panel_admin():
-    global currentpanel
-    currentpanel.pack_forget()
-
-    painel_adm = PanedWindow(window, width=1080, height=720)
-    painel_adm.place(x=0,y=0)
-    currentpanel = painel_adm
-    painel_adm.configure(bg = "#d3d3d3")
-    
-    # imagem e infos do user
-    userImg = PhotoImage(file= "./imgs/painel_adm/user.png")
-    labelImg = Label(painel_adm, image=userImg, width=180, height=180, bd=0, bg="#d3d3d3")
-    labelImg.place(x= 420, y=100)
-
-    admTxt = Label(painel_adm, text="Função: administrador", width=24, height=3, bd=0, bg="#d3d3d3", fg="black")
-    admTxt.place(x=420, y=345)
-
-    # botão voltar
-    btnVoltar = Button(painel_adm, text="Voltar", width=10, command=panel_account)
-    btnVoltar.place(x=30, y=30)
-
-    # botão add album
-    btnAddAlbum = Button(painel_adm, text="Adicione um álbum", width=25)
-    btnAddAlbum.place(x=300, y=480)
-
-    # botão remover album
-    btnRemovAlbum = Button(painel_adm, text="Remova um álbum", width=25)
-    btnRemovAlbum.place(x=520, y=480)
-
-    # botão usuarios
-    btnUsers = Button(painel_adm, text="Veja usuários", width=25)
-    btnUsers.place(x=300, y=525)
-
-    # botão apagar reviews
-    btnApagarReviews = Button(painel_adm, text="Apague um review", width=25)
-    btnApagarReviews.place(x=520, y=525)
-
-    # botão log out
-    btnLogOut = Button(painel_adm, text="Log out", width=10, command=logout_and_change_page(panel_homepage))
-    btnLogOut.place(x=960, y=670)
-
-## - - - - - - - - - - CONTAINER NOTIFICACOES - - - - - - - - - - ##
-
-def panel_notific():
-    global currentpanel
-    currentpanel.pack_forget()
-
-    panel_notific = PanedWindow(window, width=1080, height=720)
-    currentpanel = panel_notific
-    panel_notific.configure(bg = "#d3d3d3")
-
-    txt = Label(panel_notific, text="Isto é uma notificacao", width=24, height=3, bd=0, bg="#d3d3d3", fg="black")
-    txt.place(x=420, y=345) 
-
-## - - - - - - - - - - CONTAINER SEARCH - - - - - - - - - - ##
-
-def panel_search():
-    global currentpanel
-    currentpanel.pack_forget()
-
-    panel_search = PanedWindow(window, width=1080, height=720)
-    currentpanel = panel_search
-    panel_search.configure(bg = "#d3d3d3")
-
-
-    txt = Label(panel_notific, text="Pesquise aqui", width=24, height=3, bd=0, bg="#d3d3d3", fg="black")
-    txt.place(x=420, y=345)
 
 
 

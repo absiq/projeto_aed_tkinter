bc35248a7366fef7dae0bdfe210ddcdf9f322073
Beatriz Lisboa <btrzlisboa037@gmail.com>
2023-01-07 17:11:14 +0000

    adc botoes login e registro, link paineis + bg

--- main.py
+++ main.py
@@ -16,17 +16,6 @@ def login_and_change_page(username, password, page):
 
 ## - - - - - - - - - - CONTAINER REGISTER - - - - - - - - - - ##
 
-def panel_admin():
-    global currentpanel
-    currentpanel.pack_forget()
-
-    window_admin = PanedWindow(window, width=1080, height=720)
-    currentpanel = window_admin
-
-    label_admin = Label(window_admin, text="Bem-vindo, administrador!", width=34)
-    label_admin.place(x=400, y=200)
-
-    window_admin.place(x=0, y=0)
 
 def panel_edit_profile():
     global currentpanel
@@ -35,24 +24,26 @@ def panel_edit_profile():
     window_edit_profile = PanedWindow(window, width=1080, height=720)
     currentpanel = window_edit_profile
 
+    window_edit_profile.configure(bg = "#d3d3d3")
+
     name, username, icon, bio = retrieve_current_user_data()
 
     name_text = "Teu nome atual é: " + name
-    label_name = Label(window_edit_profile, text=name_text)
+    label_name = Label(window_edit_profile, text=name_text, bg = "#d3d3d3")
     label_name.place(x=20, y=50)
 
     username_text = "Teu username atual é: " + username
-    label_username = Label(window_edit_profile, text=username_text)
+    label_username = Label(window_edit_profile, text=username_text, bg = "#d3d3d3")
     label_username.place(x=400, y=50)
 
     bio_text = "Tua bio atual é: "
-    label_bio_text = Label(window_edit_profile, text=bio_text)
-    label_bio = Label(window_edit_profile, justify='center', wraplength=240, text=bio)
+    label_bio_text = Label(window_edit_profile, text=bio_text, bg = "#d3d3d3")
+    label_bio = Label(window_edit_profile, justify='center', wraplength=240, text=bio, bg = "#d3d3d3")
     label_bio_text.place(x=20, y=220)
     label_bio.place(x=20, y=240)
 
     icon_text = "O teu icon atual é: "
-    label_icon_text = Label(window_edit_profile, text=icon_text)
+    label_icon_text = Label(window_edit_profile, text=icon_text,bg = "#d3d3d3")
     label_icon_text.place(x=740, y=220)
     ficheiro_img = os.path.join('imgs\\profile_pics', icon)
     user_img = Canvas(window_edit_profile, width=100, height=100, bd=0)
@@ -61,19 +52,19 @@ def panel_edit_profile():
     img = PhotoImage(file = ficheiro_img)
     user_img.create_image(50,50, anchor=CENTER, image = img)
 
-    label_new_name = Label(window_edit_profile, text="Insira o teu novo nome:")
+    label_new_name = Label(window_edit_profile, text="Insira o teu novo nome:", bg = "#d3d3d3")
     label_new_name.place(x=20, y=80)
     new_name = name
     entry_new_name = Entry(window_edit_profile, width=30, textvariable=new_name)
     entry_new_name.place(x=20, y=100)
 
-    label_new_username = Label(window_edit_profile, text="Insira o teu novo username:")
+    label_new_username = Label(window_edit_profile, text="Insira o teu novo username:", bg = "#d3d3d3")
     label_new_username.place(x=400, y=80)
     new_username = username
     entry_new_username = Entry(window_edit_profile, width=30, textvariable=new_username)
     entry_new_username.place(x=400, y=100)
 
-    label_new_bio = Label(window_edit_profile, text="Insira a tua nova bio:")
+    label_new_bio = Label(window_edit_profile, text="Insira a tua nova bio:", bg = "#d3d3d3")
     label_new_bio.place(x=20, y=290)
     new_bio = bio
     entry_new_bio = Text(window_edit_profile) # usar GET para inserir conteúdo do Text em new_bio
@@ -89,34 +80,39 @@ def panel_register():
     window_register.place(x=0, y=0)
     currentpanel = window_register
 
+    window_register.configure(bg = "#d3d3d3")
+
     ficheiro_img = os.path.join('imgs\profile_pics', 'avatarnone.png')
 
     register_img = Canvas(window_register, width=100, height=100, bd=0)
-    register_img.place(x=510, y=60)
+    register_img.place(x=510, y=70)
     global img
     img = PhotoImage(file = ficheiro_img)
     register_img.create_image(50,50, anchor=CENTER, image = img)
 
+    lblTitulo = Label(window_register, text = "Registro", fg = "black", bg = "#d3d3d3", font = "arial, 15", relief = "flat")
+    lblTitulo.place (x=520, y=25)
+
     name_register = StringVar()
-    label_name = Label(window_register, text="Nome completo:")
+    label_name = Label(window_register, text="Nome completo:", bg = "#d3d3d3")
     entry_name = Entry(window_register, width=40, textvariable=name_register)
     label_name.place(x=440, y=220)
     entry_name.place(x=440, y=245)
 
     email_register = StringVar()
-    label_email = Label(window_register, text="E-mail:")
+    label_email = Label(window_register, text="E-mail:", bg = "#d3d3d3")
     entry_email = Entry(window_register, width=40, textvariable=email_register)
     label_email.place(x=440, y=290)
     entry_email.place(x=440, y=315)
 
     username_register = StringVar()
-    label_username = Label(window_register, text="Nome de usuário:")
+    label_username = Label(window_register, text="Nome de usuário:", bg = "#d3d3d3")
     entry_username = Entry(window_register, width=40, textvariable=username_register)
     label_username.place(x=440, y=360)
     entry_username.place(x=440, y=385)
 
     password_register = StringVar()
-    label_password = Label(window_register, text="Password:")
+    label_password = Label(window_register, text="Password:", bg = "#d3d3d3")
     entry_password = Entry(window_register, width=40, textvariable=password_register, show='*')
     label_password.place(x=440, y=430)
     entry_password.place(x=440, y=455)
@@ -135,22 +131,27 @@ def panel_login():
     window_login = PanedWindow(window, width=1080, height=720)
     currentpanel = window_login
 
+    window_login.configure(bg = "#d3d3d3")
+
     ficheiro_img = os.path.join('imgs\profile_pics', 'avatarnone.png')
 
-    register_img = Canvas(window_login, width=100, height=100, bd=0)
-    register_img.place(x=510, y=60)
+    register_img = Canvas(window_login, width=100, height=100, bd=0, bg = "#d3d3d3")
+    register_img.place(x=510, y=70)
     global img
     img = PhotoImage(file = ficheiro_img)
     register_img.create_image(50,50, anchor=CENTER, image = img)
 
+    lblTitulo = Label(window_login, text = "Log in", fg = "black", bg = "#d3d3d3", font = "arial, 15", relief = "flat")
+    lblTitulo.place (x=530, y=25)
+
     username_login = StringVar()
-    label_username = Label(window_login, text="Nome de usuário:")
+    label_username = Label(window_login, text="Nome de usuário:", bg = "#d3d3d3")
     entry_username = Entry(window_login, width=40, textvariable=username_login)
     label_username.place(x=440, y=220)
     entry_username.place(x=440, y=245)
 
     password_login = StringVar()
-    label_password = Label(window_login, text="Password:")
+    label_password = Label(window_login, text="Password:", bg = "#d3d3d3")
     entry_password = Entry(window_login, width=40, textvariable=password_login, show='*')
     label_password.place(x=440, y=290)
     entry_password.place(x=440, y=315)
@@ -158,8 +159,11 @@ def panel_login():
     btn_submit_login = Button(window_login, text="Login", width=34, command= lambda: login_and_change_page(username_login.get(), password_login.get(), panel_account))
     btn_submit_login.place(x=440, y=380)
 
+    btn_register = Button(window_login, text="Não tens conta? Registre-se", width=34, command= panel_register)
+    btn_register.place(x=440, y=440)
+
     btn_voltar = Button(window_login, text="Voltar", width=34, command=panel_homepage)
-    btn_voltar.place(x=440, y=455)
+    btn_voltar.place(x=440, y=500)
 
     window_login.place(x=0, y=0)
 
@@ -172,6 +176,8 @@ def panel_account():
     window_account = PanedWindow(window, width=1080, height=720)
     currentpanel = window_account
 
+    window_account.configure(bg = "#d3d3d3")
+
     name, username, icon, bio = retrieve_current_user_data()
 
     ficheiro_img = os.path.join('imgs\\profile_pics', icon)
@@ -182,17 +188,17 @@ def panel_account():
     img = PhotoImage(file = ficheiro_img)
     register_img.create_image(50,50, anchor=CENTER, image = img)
 
-    label_username = Label(window_account, text=name, font=("Helvetica", 24))
+    label_username = Label(window_account, text=name, font=("Helvetica", 24), bg = "#d3d3d3")
     label_username.place(x=220, y=40)
 
     text_label_username = '@' + username
-    label_username = Label(window_account, text=text_label_username, font=("Helvetica", 12))
+    label_username = Label(window_account, text=text_label_username, font=("Helvetica", 12), bg = "#d3d3d3")
     label_username.place(x=220, y=90)
 
-    label_username = Label(window_account, text=bio, justify='center', wraplength=240)
+    label_username = Label(window_account, text=bio, justify='center', wraplength=240, bg = "#d3d3d3")
     label_username.place(x=100, y=160)
 
-    btn_edit_profile = Button(window_account, text="Editar perfil", width=20, command=panel_edit_profile)
+    btn_edit_profile = Button(window_account, text="Editar perfil", width=20, bg = "#d3d3d3", command=panel_edit_profile)
     btn_edit_profile.place(x=800, y=40)
 
     btn_logout = Button(window_account, text="Fazer logout", width=20, command= lambda: logout_and_change_page(panel_homepage))
@@ -223,7 +229,7 @@ def panel_homepage():
 
     #define icone da lupa no botao de busca
     imgSearch = PhotoImage(file = "./imgs/home/search.png", height=20, width=20)
-    btnGuardarS = Button (home_page, width = 40, height = 40, image = imgSearch, border=0, bg="#d3d3d3")
+    btnGuardarS = Button (home_page, width = 40, height = 40, image = imgSearch, border=0, bg="#d3d3d3", command=panel_search)
     btnGuardarS.place (x = 1020 , y = 7)
 
     #define icone de user no botao para ir p/ página do usuário
@@ -233,7 +239,7 @@ def panel_homepage():
 
     #define icone de sino para ir pra página de notificações
     imgNotific = PhotoImage(file = "./imgs/home/sino.png", height=20, width=20)
-    btnGuardarN = Button (home_page, width = 40, height = 40, image = imgNotific, border=0, bg="#d3d3d3")
+    btnGuardarN = Button (home_page, width = 40, height = 40, image = imgNotific, border=0, bg="#d3d3d3", command=panel_notific)
     btnGuardarN.place (x = 930 , y = 9)
 
     #define area de destaques
@@ -274,6 +280,7 @@ def panel_homepage():
 
     home_page.mainloop()
 
+## - - - - - - - - - - CONTAINER ADMIN PANEL - - - - - - - - - - ##
 
 def panel_admin():
     global currentpanel
@@ -282,6 +289,7 @@ def panel_admin():
     painel_adm = PanedWindow(window, width=1080, height=720)
     painel_adm.place(x=0,y=0)
     currentpanel = painel_adm
+    painel_adm.configure(bg = "#d3d3d3")
     
     admTxt = Label(painel_adm, text="Função: administrador", width=24, height=3, bd=0, bg="#d3d3d3", fg="black")
     admTxt.place(x=420, y=345)
@@ -314,13 +322,32 @@ def panel_admin():
     btnLogOut = Button(painel_adm, text="Log out", width=10, command=logout_and_change_page(panel_homepage))
     btnLogOut.place(x=960, y=670)
 
+## - - - - - - - - - - CONTAINER NOTIFICACOES - - - - - - - - - - ##
+
 def panel_notific():
-    tituloA1 ="This is a notificacao"
-    tituloA1.place(x=210, y=345)
+    global currentpanel
+    currentpanel.pack_forget()
+
+    panel_notific = PanedWindow(window, width=1080, height=720)
+    currentpanel = panel_notific
+    panel_notific.configure(bg = "#d3d3d3")
+
+    txt = Label(panel_notific, text="Isto é uma notificacao", width=24, height=3, bd=0, bg="#d3d3d3", fg="black")
+    txt.place(x=420, y=345) 
+
+## - - - - - - - - - - CONTAINER SEARCH - - - - - - - - - - ##
 
 def panel_search():
-    tituloA1 ="Pesquise aqui"
-    tituloA1.place(x=210, y=345)
+    global currentpanel
+    currentpanel.pack_forget()
+
+    panel_search = PanedWindow(window, width=1080, height=720)
+    currentpanel = panel_search
+    panel_search.configure(bg = "#d3d3d3")
+
+
+    txt = Label(panel_notific, text="Pesquise aqui", width=24, height=3, bd=0, bg="#d3d3d3", fg="black")
+    txt.place(x=420, y=345)
 
 
 
